3bb6a17cc6c43519ef1537d32c7486fbe500a430
tartagliajk <92087041+tartagliajk@users.noreply.github.com>
2023-06-01 21:48:16 +0200

    Add files via upload

---
+++ code/ChangePassword.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace recycling
+{
+    public partial class ChangePassword : Form
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void logInSubmit_Click(object sender, EventArgs e)
+        {
+            submit.Visible = false;
+            var writtenName = nameBox.Text;
+            var writtenPassword = passwordBox.Text;
+
+            var users = File.ReadAllText(@"C:\Users\nina.vazae\Desktop\Recycling-main\code\detailes.txt").Split(',').ToList().Where(x => !String.IsNullOrWhiteSpace(x));
+
+            List<User> myUsers = new List<User>();
+            foreach (var user in users)
+            {
+                var information = user.Split(' ');
+                User temp = new User();
+                temp.Name = information[0].Trim();
+                temp.Password = information[1].Trim();
+                temp.status = information[2].Trim();
+                myUsers.Add(temp);
+            }
+
+            var selectedUser = myUsers.Where(x => x.Name == writtenName).SingleOrDefault();
+            myUsers.Remove(selectedUser);
+            selectedUser.Password = writtenPassword;
+            myUsers.Add(selectedUser);
+
+            List<string> formatForFile = new List<string>();
+            foreach (var item in myUsers)
+            {
+                formatForFile.Add(item.PrepareForFile());
+            }
+            File.WriteAllLines(@"C:\Users\nina.vazae\Desktop\Recycling-main\code\detailes.txt", formatForFile.ToArray());
+
+            label2.Text = "The password has been changed";
+        }
+
+        private void returnText_Click(object sender, EventArgs e)
+        {
+            startSite startsite = new startSite();
+            startsite.Show();
+            Hide();
+        }
+
+        private class User
+        {
+            public string Name { get; set; }
+            public string Password { get; set; }
+            public string status { get; set; }
+
+            public string PrepareForFile()
+            {
+                return Name + " " + Password + " " + status + ",";
+            }
+        }
+    }
+}
---
+++ code/Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static recycling.startSite;
+
+namespace recycling
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            File.WriteAllText("C:\\Users\\nina.vazae\\Desktop\\Recycling-main\\code\\trashcans.txt", String.Empty);
+            InitializeComponent();
+            inputPanel.Visible = false;
+            trashDisplay.Visible = false;
+        }
+
+        public class systemCreate
+        {
+            public static trashCanSystem system = trashCanSystem.GetInstance();
+        }
+
+        private void startBut_Click(object sender, EventArgs e)
+        {
+            inputPanel.Visible = true;
+            startBut.Visible = false;
+        }
+
+        private void sumbit_Click(object sender, EventArgs e)
+        {
+            Globals.i++;
+            void addCan()
+            {
+                var id = (systemCreate.system.getCans().Count + 1).ToString();
+                string name = nameBox.Text;
+                string item = trashBox.Text;
+                string color = colorBox.Text;
+
+                trashCan newTrash = new trashCan(id, name, color, item);
+                systemCreate.system.addCan(newTrash);
+            }
+            addCan();
+            inputPanel.Visible = false;
+            trashDisplay.Visible = true;
+            List<trashCan> trashCans = systemCreate.system.getCans();
+            trashCan trashCan = trashCans[Globals.i];
+            nameDisplay.Text = $"Name: {trashCan.Name}";
+            itemDisplay.Text = $"Item: {trashCan.Item}";
+            colorDisplay.Text = $"Color: {trashCan.Color}";
+        }
+
+        private void returnStartM_Click(object sender, EventArgs e)
+        {
+            MenuSelect menuselect = new MenuSelect();
+            menuselect.Show();
+            Hide();
+        }
+    }
+}
